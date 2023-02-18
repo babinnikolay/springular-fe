de50905060832df0caa01a1dc09f6098c8cc862a
Babin Nikolay <babin.nik@gmail.com>
2023-02-18 20:04:15 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IProduct} from "./models/product";
+
+import {ProductService} from "./services/product.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'springular';
+
+  products: IProduct[] = []
+
+  constructor(private productsService: ProductService) {
+  }
+  ngOnInit(): void {
+    this.productsService.findAll().subscribe(products => {
+      this.products = products
+    })
+  }
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {IProduct} from "../models/product";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService{
+  constructor(private http: HttpClient) {
+  }
+
+  findAll(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>('http://localhost:8080/products')
+  }
+}
